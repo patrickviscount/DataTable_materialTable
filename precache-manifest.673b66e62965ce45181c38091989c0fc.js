d27ec8be93ae72877eef66cc192600c2315b3e0d
PT <patviscount@gmail.com>
2022-04-11 15:25:23 -0300

    Updates

---
+++ precache-manifest.673b66e62965ce45181c38091989c0fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea521ece0a48578640d4db8f8e242a14",
+    "url": "/DataTable_materialTable/index.html"
+  },
+  {
+    "revision": "47605b7dbcdd7ed75c1b",
+    "url": "/DataTable_materialTable/static/css/main.f6619852.chunk.css"
+  },
+  {
+    "revision": "7261b5ff953d4b56dd38",
+    "url": "/DataTable_materialTable/static/js/2.308147ce.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/DataTable_materialTable/static/js/2.308147ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dead8b44ed13bae0f36",
+    "url": "/DataTable_materialTable/static/js/3.723552d5.chunk.js"
+  },
+  {
+    "revision": "ab427901b2b00e9b729241987226eed9",
+    "url": "/DataTable_materialTable/static/js/3.723552d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d146eb051a56dbbb50b",
+    "url": "/DataTable_materialTable/static/js/4.73c9fca0.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/DataTable_materialTable/static/js/4.73c9fca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e634bf45b6180464f92c",
+    "url": "/DataTable_materialTable/static/js/5.ce3af5bc.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/DataTable_materialTable/static/js/5.ce3af5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47605b7dbcdd7ed75c1b",
+    "url": "/DataTable_materialTable/static/js/main.26369413.chunk.js"
+  },
+  {
+    "revision": "2aca02bd4e2f874c4210",
+    "url": "/DataTable_materialTable/static/js/runtime-main.f664abc7.js"
+  }
+]);
